275e410488386893fe2ee57e8ee98e87b37e6fba
Boris <fjmiranda009@gmail.com>
2020-04-15 11:48:31 -0600

    agregando las plantillas

---
+++ Sistema - Agencia/AdminLTE-master/plantillas/barra_lateral.php
@@ -0,0 +1,274 @@
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="../index.html" class="brand-link">
+      <span class="brand-text font-weight-light">Agencia de Viajes</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+            <img src="../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Alexander Pierce</a>
+        </div>
+      </div>
+  
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+         
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-users"></i>
+              <p>
+                Clientes
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                  <a href="../pages/forms/cliente.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Registro de Cliente</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                  <a href="../pages/forms/advanced.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Catalogo de Clientes</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-header">SERVICIOS</li>
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-map-marked-alt"></i>
+              <p>
+                Paquetes
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                  <a href="../pages/forms/general.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Registro de Paquetes</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                  <a href="../pages/forms/advanced.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Disponibilidad de Reserva</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-umbrella-beach"></i>
+              <p>
+                Tours
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                  <a href="../pages/forms/general.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Registro de Tours</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                  <a href="../pages/forms/advanced.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Disponibilidad de Reserva</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-car"></i>
+              <p>
+                Vehiculos
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                  <a href="../pages/forms/general.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Registro de Vehiculo</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                  <a href="../pages/forms/advanced.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Disponibilidad de Reserva</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                  <a href="../pages/forms/advanced.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Mantenimiento</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-mail-bulk"></i>
+              <p>
+                Cargo Expreso
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                  <a href="../pages/forms/general.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Registro de Encomienda</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                  <a href="../pages/forms/advanced.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Calculo de Costo</p>
+                </a>
+              </li>
+              
+            </ul>
+          </li>
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-balance-scale-right"></i>
+              <p>
+                Asesoría Migratoria
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                  <a href="../pages/forms/general.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Generar Cita</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                  <a href="../pages/forms/advanced.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Llenado de Formulario</p>
+                </a>
+              </li>
+              
+            </ul>
+          </li>
+          <li class="nav-header">COTIZACIONES</li>
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-file-invoice-dollar"></i>
+              <p>
+                Cotización
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                  <a href="../pages/forms/general.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Cotización de Vuelo</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                  <a href="../pages/forms/general.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Cotización de Paquete</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                  <a href="../pages/forms/general.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Cotización de Tours</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                  <a href="../pages/forms/general.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Cotización de Vehiculo</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-header">CONTROL</li>
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-address-card"></i>
+              <p>
+                Contactos
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                  <a href="../pages/forms/general.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Registro de Proveedor</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-hand-holding-usd"></i>
+              <p>
+                Ingresos
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                  <a href="../pages/forms/general.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Estadisticas</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-users-cog"></i>
+              <p>
+                Usuarios
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                  <a href="../pages/forms/general.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Registrar Usuario</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                  <a href="../pages/forms/general.html" class="nav-link">
+                  <i class="fas fa-ellipsis-v nav-icon"></i>
+                  <p>Bitácora</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+         
+  
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
