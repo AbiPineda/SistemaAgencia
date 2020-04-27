e5c720a5daf3ee58be7382d807dd69c20df58cf1
JcMoz <31970838+JcMoz@users.noreply.github.com>
2020-04-27 14:40:33 -0600

    Detalle_actu_envio

--- SistemaAgencia/vistas/cargoExpress/actualizacionEnvio.php
+++ SistemaAgencia/vistas/cargoExpress/actualizacionEnvio.php
@@ -1,13 +1,13 @@
 <!--para las tablas que cargan en este archivo-->
-<?php 
+<?php
 include_once '../../config/parametros.php';
 ?>
-  <link rel="stylesheet" href="<?=$base_url?>plugins/fontawesome-free/css/all.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- DataTables -->
-    <link rel="stylesheet" href="<?=$base_url?>plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="<?=$base_url?>plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+<link rel="stylesheet" href="<?= $base_url ?>plugins/fontawesome-free/css/all.min.css">
+<!-- Ionicons -->
+<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+<!-- DataTables -->
+<link rel="stylesheet" href="<?= $base_url ?>plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="<?= $base_url ?>plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
 
 <!--****************fin****************-->
 
@@ -19,7 +19,7 @@ include_once '../../config/parametros.php';
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-5" id="titulo">
-                    <h1>Registro de Actualizacion de Envio</h1>
+                    <h1>Registro de Actualización de Envio</h1>
                 </div>
                 <div class="col-sm-3" id="titulo">
                     <div class="center">
@@ -71,12 +71,12 @@ include_once '../../config/parametros.php';
 
                                         <input type="hidden" id="taskId">
                                         <div >
-                                        <button style="margin: 5px" type="submit"
-                                            class="btn btn-default float-right">Cancelar</button>
-                                        <button style="margin: 5px" type="submit"
-                                            class="btn btn-info float-right">Guardar</button>
+                                            <button style="margin: 5px" type="submit"
+                                                    class="btn btn-default float-right">Cancelar</button>
+                                            <button style="margin: 5px" type="submit"
+                                                    class="btn btn-info float-right">Guardar</button>
 
-                                    </div>
+                                        </div>
                                     </form>
                                 </div>
                             </div>
@@ -93,50 +93,72 @@ include_once '../../config/parametros.php';
                                         <label>Buscar Encomienda</label>
                                         <input type="text" class="form-control" placeholder="Ingrese la Encomienda a buscar">
                                     </div>
+                                    
+                                    <div id="mover_tablas">
+                                    <table id="example2" class="table table-bordered table-hover">
+                                        <thead>
+                                            <tr>
+                                                <th>Fecha</th>
+                                                <th>Hora</th>
+                                                <th>Lugar</th>
+                                                <th>Descripción</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>20/04/2019</td>
+                                                <td>8:00pm</td>
+                                                <td>San Vicente</td>
+                                                <td>Esta bien</td>
+
+                                            </tr>
+                                            <tr>
+                                                <td>20/04/2019</td>
+                                                <td>8:00pm</td>
+                                                <td>San Vicente</td>
+                                                <td>Esta bien</td>
+
+                                            </tr>
+                                            <tr>
+                                                <td>20/04/2019</td>
+                                                <td>8:00pm</td>
+                                                <td>San Vicente</td>
+                                                <td>Esta bien</td>
+
+                                            </tr>
+                                            <tr>
+                                                <td>20/04/2019</td>
+                                                <td>8:00pm</td>
+                                                <td>San Vicente</td>
+                                                <td>Esta bien</td>
+
+                                            </tr>
+                                              <tr>
+                                                <td>20/04/2019</td>
+                                                <td>8:00pm</td>
+                                                <td>San Vicente</td>
+                                                <td>Esta bien</td>
+
+                                            </tr>
+                                              <tr>
+                                                <td>20/04/2019</td>
+                                                <td>8:00pm</td>
+                                                <td>San Vicente</td>
+                                                <td>Esta bien</td>
+
+                                            </tr>
+                                        </tbody>
+
+                                    </table> 
+                                        </div>
+
+
+
                                 </div>
                             </div>
-                            
-           <table id="example2" class="table table-bordered table-hover">
-                <thead>
-                <tr>
-                  <th>Fecha</th>
-                  <th>Hora</th>
-                  <th>Lugar</th>
-                  <th>Descripción</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                  <td>20/04/2019</td>
-                  <td>8:00pm</td>
-                  <td>San Vicente</td>
-                  <td>Esta bien</td>
-                
-                </tr>
-                <tr>
-                  <td>20/04/2019</td>
-                  <td>8:00pm</td>
-                  <td>San Vicente</td>
-                  <td>Esta bien</td>
-                
-                </tr>
-                <tr>
-                  <td>20/04/2019</td>
-                  <td>8:00pm</td>
-                  <td>San Vicente</td>
-                  <td>Esta bien</td>
-                
-                </tr>
-                <tr>
-                  <td>20/04/2019</td>
-                  <td>8:00pm</td>
-                  <td>San Vicente</td>
-                  <td>Esta bien</td>
-                
-                </tr>
-                </tbody>
-                
-              </table>     
+
+
+
 
                         </div>
                     </div>
@@ -152,56 +174,56 @@ include_once '../../config/parametros.php';
 </section>
 <!-- /.content -->
 </div>
-<script src="<?=$base_url?>plugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="<?=$base_url?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- DataTables -->
-    <script src="<?=$base_url?>plugins/datatables/jquery.dataTables.min.js"></script>
-    <script src="<?=$base_url?>plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-    <script src="<?=$base_url?>plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="<?=$base_url?>plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="<?=$base_url?>dist/js/adminlte.min.js"></script>
-    <!-- AdminLTE for demo purposes -->
-   <script>
-  $(function () {
-    $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false,
-      "pageLength":3,
-      "responsive": true,
-      "oLanguage": {    
-		    "sZeroRecords":    "No se encontraron resultados",
-		    "sEmptyTable":     "Ningún dato disponible en esta tabla",
-		    "sInfo":           "Mostrando del (_START_ al _END_) de _TOTAL_ registros",
-		    "sInfoEmpty":      "Mostrando del 0 al 0  de 0 registros",
-		    "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-		    "sInfoPostFix":    "",
-		    "sSearch":         "Filtrar:",
-		    "sUrl":            "",
-		    "sInfoThousands":  ",",
-		    "sLoadingRecords": "Por favor espere - cargando...",
-		    "oPaginate": {
-		        "sFirst":    "Primero",
-		        "sLast":     "Último",
-		        "sNext":     "Siguiente",
-		        "sPrevious": "Anterior"
-		    },
-		    "oAria": {
-		        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-		        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-		    }
-        }
+<script src="<?= $base_url ?>plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?= $base_url ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- DataTables -->
+<script src="<?= $base_url ?>plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="<?= $base_url ?>plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="<?= $base_url ?>plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="<?= $base_url ?>plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?= $base_url ?>dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script>
+    $(function () {
+        $('#example2').DataTable({
+            "paging": true,
+            "lengthChange": false,
+            "searching": false,
+            "ordering": true,
+            "info": true,
+            "autoWidth": false,
+            "pageLength": 5,
+            "responsive": true,
+            "oLanguage": {
+                "sZeroRecords": "No se encontraron resultados",
+                "sEmptyTable": "Ningún dato disponible en esta tabla",
+                "sInfo": "Mostrando del (_START_ al _END_) de _TOTAL_ registros",
+                "sInfoEmpty": "Mostrando del 0 al 0  de 0 registros",
+                "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+                "sInfoPostFix": "",
+                "sSearch": "Filtrar:",
+                "sUrl": "",
+                "sInfoThousands": ",",
+                "sLoadingRecords": "Por favor espere - cargando...",
+                "oPaginate": {
+                    "sFirst": "Primero",
+                    "sLast": "Último",
+                    "sNext": "Siguiente",
+                    "sPrevious": "Anterior"
+                },
+                "oAria": {
+                    "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                }
+            }
+        });
     });
-  });
 </script>
 
 <!-- /.content-wrapper -->
 <!--para la paginacion de las tablas-->
-  
+
 
 <!--fin de la paginacion de las tablas-->
