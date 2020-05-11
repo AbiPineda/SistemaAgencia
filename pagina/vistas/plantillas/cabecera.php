003ce112df8e0c3d5826c2a740a8ea73cd0e9162
Boris <fjmiranda009@gmail.com>
2020-05-11 14:27:36 -0600

    agregando plantillas

---
+++ pagina/vistas/plantillas/cabecera.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<head>
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="img/fav.png">
+    <!-- Author Meta -->
+    <meta name="author" content="colorlib">
+    <!-- Meta Description -->
+    <meta name="description" content="">
+    <!-- Meta Keyword -->
+    <meta name="keywords" content="">
+    <!-- meta character set -->
+    <meta charset="UTF-8">
+    <!-- Site Title -->
+    <title>Travel</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet">
+    <!--
+        CSS
+        ============================================= -->
+    <link rel="stylesheet" href="<?=$base_url?>css/linearicons.css">
+    <link rel="stylesheet" href="<?=$base_url?>css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?=$base_url?>css/bootstrap.css">
+    <link rel="stylesheet" href="<?=$base_url?>css/magnific-popup.css">
+    <link rel="stylesheet" href="<?=$base_url?>css/jquery-ui.css">
+    <link rel="stylesheet" href="<?=$base_url?>css/nice-select.css">
+    <link rel="stylesheet" href="<?=$base_url?>css/animate.min.css">
+    <link rel="stylesheet" href="<?=$base_url?>css/owl.carousel.css">
+    <link rel="stylesheet" href="<?=$base_url?>css/main.css">
+</head>
+
+<body>
