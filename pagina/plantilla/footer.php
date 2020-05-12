9331bcdb35a7aff543215902c479a18dcace6d0a
JcMoz <31970838+JcMoz@users.noreply.github.com>
2020-05-12 09:08:24 -0600

    plantilla_pagina

---
+++ pagina/plantilla/footer.php
@@ -0,0 +1,152 @@
+       <!-- Start Footer bottom Area -->
+        <footer  id="contact">
+            <div class="footer-area">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-4 col-xs-12">
+                            <div class="footer-content">
+                                <div class="footer-head">
+                                    <div class="footer-logo">
+                                        <h2><span>Martínez</span>Travels & Tours</h2>
+                                    </div>
+
+                                    <p>Siguenos en todas nuestras redes sociales para estar al tanto de todas nuestras promociones y productos</p>
+                                    <div class="footer-icons">
+                                        <ul>
+                                            <li>
+                                                <a href="#"><i class="fa fa-facebook"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="#"><i class="fa fa-twitter"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="#"><i class="fa fa-google"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="#"><i class="fa fa-pinterest"></i></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end single footer -->
+                        <div class="col-md-4 col-sm-4 col-xs-12">
+                            <div class="footer-content">
+                                <div class="footer-head">
+                                    <div class="footer-logo">
+                                        <h2><span>Cont</span>acto</h2>
+                                    </div>
+                                    <p>
+                                        Tambien puedes contactarnos de las siguiente manera 
+                                    </p>
+                                    <div class="footer-contacts">
+                                        <p><span>Tel:</span>+(503) 7841-1184</p>
+                                        <p><span>Email:</span>info.ventas@martineztraveltours.com</p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end single footer -->
+                        <div class="col-md-4 col-sm-4 col-xs-12">
+                            <div class="footer-content">
+                                <div class="footer-head">
+                                    <div class="footer-logo">
+                                        <h2><span>Ubic</span>ación</h2>
+                                    </div>
+                                    <p>
+                                        Ubicación: 2a Avenida Sur, Barrio el Centro #4D San Vicente, El Salvador
+                                    </p>
+                                    <div class="footer-contacts">
+
+                                        <p><span>Lunes-Viernes</span>  (8am-5:30pm)</p>
+                                        <p><span>Sábados</span> (8am-12:30pm)</p>
+                                    </div>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="container">
+                        <div class="row">
+                            <!-- Start Google Map -->
+                            <div class="col-md-12 col-sm-12 col-xs-12">
+                                <button class="btn btn-primary btn-lg btn-block" style="background: #3EC1D5; none repeat scroll 0 0; border: 1px solid #3EC1D5; " >INICIAR CHAT CON SERVICIO AL CLIENTE</button>
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <br>
+
+                    <div class="container">
+                        <div class="row">
+                            <!-- Start Google Map -->
+                            <div class="col-md-12 col-sm-12 col-xs-12">
+                                <!-- Start Map -->
+                                <iframe
+                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3877.2372796321397!2d-88.78637328568938!3d13.64332530347501!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f64a9a35e10e6d1%3A0xe126b681eebcd3f3!2sMartinez%20Travel%20%26%20Tours!5e0!3m2!1ses!2ssv!4v1581302467604!5m2!1ses!2ssv"
+                                    width="100%" height="380" frameborder="0" style="border:0" allowfullscreen></iframe>
+                                <!-- End Map -->
+                            </div>
+                            <!-- End Google Map -->
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="footer-area-bottom">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                            <div class="copyright text-center">
+                                <p>
+                                    &copy; Copyright <strong>Universidad Nacional de El Salvador</strong>
+                                </p>
+                            </div>
+                            <!--
+                            <div class="credits">
+                                
+                                  All the links in the footer should remain intact.
+                                  You can delete the links only if you purchased the pro version.
+                                  Licensing information: https://bootstrapmade.com/license/
+                                  Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=eBusiness
+                               
+                                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+                            </div>
+                            -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- end Footer bottom Area -->
+
+
+
+
+        <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+        <!-- JavaScript Libraries -->
+        <script src="lib/jquery/jquery.min.js"></script>
+        <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+        <script src="lib/venobox/venobox.min.js"></script>
+        <script src="lib/knob/jquery.knob.js"></script>
+        <script src="lib/wow/wow.min.js"></script>
+        <script src="lib/parallax/parallax.js"></script>
+        <script src="lib/easing/easing.min.js"></script>
+        <script src="lib/nivo-slider/js/jquery.nivo.slider.js" type="text/javascript"></script>
+        <script src="lib/appear/jquery.appear.js"></script>
+        <script src="lib/isotope/isotope.pkgd.min.js"></script>
+
+        <!-- Contact Form JavaScript File -->
+        <script src="contactform/contactform.js"></script>
+
+        <script src="js/main.js"></script>
+    </body>
+
+</html>
