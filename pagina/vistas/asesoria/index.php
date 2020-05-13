ed8deeca88d00dd3abc00b6977d2a606f09ca458
JcMoz <31970838+JcMoz@users.noreply.github.com>
2020-05-13 11:42:22 -0600

    estable

---
+++ pagina/vistas/asesoria/index.php
@@ -0,0 +1,125 @@
+<?php
+include_once'../../config/parametros.php';
+include_once '../../plantilla/cabecera.php';
+include_once '../../plantilla/menu.php';
+include_once '../../plantilla/portada.php';
+?>  
+
+    <!-- start banner Area -->
+        <section class="relative about-banner" style="margin-top: 14px">	
+            <div class="overlay overlay-bg"></div>
+            <div class="container">				
+                <div class="row d-flex align-items-center justify-content-center">
+                    <div class="about-content col-lg-12">
+                        <h1 class="text-white">
+                            Asesoría Migratoria				
+                        </h1>	
+                    </div>	
+                </div>
+            </div>
+        </section>
+        <!-- End banner Area -->					  
+        
+        <!-- Start post-content Area -->
+        <section class="post-content-area single-post-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 posts-list">
+                        <div class="single-post row">
+                            <div class="col-lg-12">
+                                <div class="feature-img">
+                                    <img class="img-fluid" src="../../img/asesoria/banner.png" alt="" style="max-width: 80%; height: auto;">
+                                </div>									
+                            </div>
+                            <div class="col-lg-3  col-md-3 meta-details">
+                                
+                                <div class="user-details row">
+                                    <p class="date col-lg-12 col-md-12 col-6"><a href="#">Enero, 2020</a> <span class="lnr lnr-calendar-full"></span></p>
+                                    <ul class="social-links col-lg-12 col-md-12 col-6">
+                                        <li><a href="#"><i class=""></i></a></li>
+                                    </ul>																				
+                                </div>
+                            </div>
+                            <div class="col-lg-9 col-md-9">
+                                <h3 class="mt-20 mb-20">Visados</h3>
+                                <p class="excert">
+                                 Los ciudadanos de El Salvador necesitan una visa para viajar a los Estados Unidos.  Una visa de no inmigrante es un permiso para entrar en los Estados Unidos, una vez aprobada se coloca en el pasaporte, documento para viajar emitido por las autoridades del país del solicitante Una visa americana le permite viajar hacia los Estados Unidos, pero no le garantiza su entrada ni determina la cantidad de tiempo permitido para su estadía. Los oficiales de inmigración en el puerto de entrada toman esas decisiones. </p>
+                                
+                            </div>
+                            <div class="col-lg-12">
+                                <div class="quotes">
+                                    Se suspende la entrada de extranjeros que estuvieron presentes en China, excluyendo las Regiones Autónomas Especiales de Hong Kong y Macao, dentro de los 14 días previos a su llegada al puerto de entrada en los Estados Unidos, según la Proclamación Presidencial. Si reside en China, ha viajado a China recientemente o tiene la intención de viajar a China antes de su viaje planeado a los Estados Unidos, le recomendamos que posponga su cita de entrevista de visa hasta 14 días después de su salida de China.</div>
+                                <div class="row mt-30 mb-30">
+                                    <div class="col-6">
+                                  <img class="img-fluid" src="../../img/asesoria/pasaporte.jpg" alt="" style="max-width: 39%; height: auto;">
+                                    <img class="img-fluid" src="../../img/asesoria/formulario.jpg" alt="" style="max-width: 46%; height: auto;">
+                                    </div>	
+                                    <div class="col-lg-12 mt-30">
+                                        <p>
+                                            Martinez Travel & Tours cuenta con el servicio de asesoría migratoria , llenado de formulario y programación de cita
+                                            en la embajada Americana, para que tengas la oportunidad de Viajar.
+                                            El próximo en viajar puedes ser tú.
+                                        
+                                            NOTA: No es responsabilidad de la agencia de viajes, la respuesta favorable o no del consulado americano.
+                                        </p>
+                                        <p>
+                                            🔹 Mayor información
+                                        </p>
+                                        <p>
+                                            Tel Agencia: 2319-2338 / 2312-0381
+                                        </p>
+                                    <a>
+                                        <button class="btn btn-primary" style="background: #3EC1D5; none repeat scroll 0 0; border: 1px solid #3EC1D5; " >Inicia tu proceso Aquí
+                                        </button>
+                                    </a>
+                                    
+                                     <a>
+                                        <button class="btn btn-warning" style="background: cadetblue; none repeat scroll 0 0; border: 1px solid cadetblue; " >Reserva tu cita
+                                        </button>
+                                    </a>   
+                           
+                                    </div>									
+                                </div>
+                            </div>
+                            
+                        </div>
+                        
+                    </div>
+                    
+                    <div class="col-lg-4 sidebar-widgets" style=" border: 1px solid #d0d0d0;">
+                        <div class="widget-wrap">
+                            
+                            <div class="single-sidebar-widget user-info-widget" >
+                                <img src="../../img/asesoria/logo agencia.png" alt="" style="margin-left: 156px">
+                                <a href="#"><h4>Martínez Travels & Tours</h4></a>
+                                <p>
+                                   Agencia de Viajes
+                                </p>
+                                <ul class="social-links">
+                                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fa fa-google"></i></a></li>
+                                </ul>
+                                <p>
+                                Somos una agencia especializada en todo tipo de trámites migratorios para Estados Unidos. Te presentamos una solución inmediata para cualquier tipo de caso migratorio de forma honesta y transparente. </p>
+                            </div>
+
+                            <div class="single-sidebar-widget popular-post-widget" >
+                                <h4 class="popular-title" style="background: #3ec1d5;color: #fff; text-align: center; padding: 12px 0px;">Noticias Recientes</h4>
+                                <div class="popular-post-list">
+                                    <a href="#"><h4>Comisionado de Aduanas y Protección Fronteriza de EE.UU. visitó El Salvador</h4></a>
+                                    <p>
+                                        El comisionado tuvo la oportunidad de conocer de primera mano los esfuerzos que está haciendo el gobierno de los Estados Unidos para mejorar la seguridad, impulsar el crecimiento económico y fortalecer las instituciones salvadoreñas. Visitó el Centro de Atención al Migrante, donde recibió un informe sobre las acciones que El Salvador realiza para garantizar la reinserción de personas retornadas a la vida productiva del país.</p> 
+                                    <p>A través de sus redes sociales, el Embajador Ronald Johnson explicó que la visita del comisionado Morgan “reafirma el trabajo que estamos realizando con El Salvador para proteger las fronteras y luchar contra el tráfico de personas y combatir las causas de la migración ilegal”.</p>													
+                                </div>
+                            </div>
+                            							
+                        </div>
+                    </div>
+                </div>
+            </div>	
+        </section>
+        <!-- End post-content Area -->
+<?php 
+include_once '../../plantilla/footer.php';
+ ?>
