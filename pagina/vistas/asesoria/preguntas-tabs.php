cee6fd06c730664cb324a694e3b9e9d40cc2aea0
Abigail <pineverdi@gmail.com>
2020-05-19 18:54:46 -0600

    correccion

--- pagina/vistas/asesoria/preguntas-tabs.php
+++ pagina/vistas/asesoria/preguntas-tabs.php
@@ -322,20 +322,11 @@ input[type=submit]:hover {
                                                                         </div>
                                                                         <div class="col-md-4">
                                                                             <div class="form-group">
-                                                                                <fieldset id="input1"
-                                                                                    class="clonedInput">
                                                                                     <label for="fname">¿Nombre de las
                                                                                         personas?</label>
                                                                                     <input type="text" id="name1"
                                                                                         name="name1"
-                                                                                        placeholder="Your name.."><input
-                                                                                        type="button" id="btnAdd"
-                                                                                        value="+" />
-                                                                                </fieldset>
-                                                                                <fieldset>
-
-
-                                                                                </fieldset>
+                                                                                        placeholder="Your name..">                                                                   
                                                                             </div>
                                                                         </div>
 
@@ -759,42 +750,7 @@ input[type=submit]:hover {
 
 
 <script>
-$(document).ready(function() {
-    $('#btnDel').attr('disabled', 'disabled');
-    $('#btnAdd').click(function() {
-        var num = $('.clonedInput').length; // how many "duplicatable" input fields we currently have
-        var newNum = new Number(num + 1); // the numeric ID of the new input field being added
 
-        // create the new element via clone(), and manipulate it's ID using newNum value
-        var newElem = $('#input' + num).clone().attr('id', 'Add' + newNum);
-
-        // manipulate the name/id values of the input inside the new element
-        newElem.children(':last').attr('id', 'name' + newNum).attr('name', 'name' + newNum);
-
-        // insert the new element after the last "duplicatable" input field
-        $('#input' + num).after(newElem);
-
-        // enable the "remove" button
-        $('#btnDel').attr('disabled', false);
-
-        // business rule: you can only add 10 names
-        if (newNum == 10)
-            $('#btnAdd').attr('disabled', 'disabled');
-    });
-
-    $('#btnDel').click(function() {
-        var num = $('.clonedInput').length; // how many "duplicatable" input fields we currently have
-        $('#input' + num).remove(); // remove the last element
-
-        // enable the "add" button
-        $('#btnAdd').attr('disabled', false);
-
-        // if only one element remains, disable the "remove" button
-        if (num - 1 == 1)
-            $('#btnDel').attr('disabled', 'disabled');
-    });
-
-});
 </script>
 
 
