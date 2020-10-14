c3d79581da99cbed6c23e0387f1c455c71d4b037
Abigail <pineverdi@gmail.com>
2020-10-14 16:50:36 -0600

    modales de marca, modelo, categoria

--- SistemaAgencia/vistas/vehiculos/mostrarRegistros.php
+++ SistemaAgencia/vistas/vehiculos/mostrarRegistros.php
@@ -5,12 +5,12 @@ include_once  '../../plantillas/navbar.php';
 include_once '../../plantillas/barra_lateral.php';
 ?>
 <style>
-    .center {
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        width: 75%;
-    }
+.center {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 75%;
+}
 </style>
 <div class="content-wrapper" style="min-height: 1185.73px;">
     <!-- Content Header (Page header) -->
@@ -39,37 +39,48 @@ include_once '../../plantillas/barra_lateral.php';
                 <div class="card">
                     <div class="card-header">
                         <h3 class="card-title">Datos Generales</h3>
+                        <div class="timeline-footer" style="text-align: right;">
+                            <a class="btn btn-info btn-sm" style="color: white" data-toggle="modal"
+                                data-target="#modal-marca">Nueva Marca</a>
+                            <a class="btn btn-info btn-sm" style="color: white" data-toggle="modal"
+                                data-target="#modal-modelo">Nuevo Modelo</a>
+                            <a class="btn btn-info btn-sm" style="color: white" data-toggle="modal"
+                                data-target="#modal-categoria">Nueva Categoria</a>
+
+                        </div>
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body">
                         <div id="tabla_vehiculos1_wrapper" class="dataTables_wrapper dt-bootstrap4">
                             <div class="row">
                                 <div class="col-sm-12">
-                                    <table id="tabla_vehiculos1" class="table table-bordered table-striped dataTable dtr-inline"
-                                           role="grid" aria-describedby="tabla_vehiculos1_info">
+                                    <table id="tabla_vehiculos1"
+                                        class="table table-bordered table-striped dataTable dtr-inline" role="grid"
+                                        aria-describedby="tabla_vehiculos1_info">
                                         <thead>
                                             <tr role="row" style="text-align: center;">
                                                 <th class="sorting_asc" tabindex="0" aria-controls="tabla_vehiculos1"
                                                     rowspan="1" colspan="1" aria-sort="ascending"
                                                     aria-label="Rendering engine: activate to sort column descending">
                                                     Marca</th>
-                                                <th class="sorting" tabindex="0" aria-controls="tabla_vehiculos1" rowspan="1"
-                                                    colspan="1" aria-label="Browser: activate to sort column ascending">
+                                                <th class="sorting" tabindex="0" aria-controls="tabla_vehiculos1"
+                                                    rowspan="1" colspan="1"
+                                                    aria-label="Browser: activate to sort column ascending">
                                                     Modelo</th>
-                                                <th class="sorting" tabindex="0" aria-controls="tabla_vehiculos1" rowspan="1"
-                                                    colspan="1"
+                                                <th class="sorting" tabindex="0" aria-controls="tabla_vehiculos1"
+                                                    rowspan="1" colspan="1"
                                                     aria-label="Platform(s): activate to sort column ascending">
                                                     Año</th>
-                                                <th class="sorting" tabindex="0" aria-controls="tabla_vehiculos1" rowspan="1"
-                                                    colspan="1"
+                                                <th class="sorting" tabindex="0" aria-controls="tabla_vehiculos1"
+                                                    rowspan="1" colspan="1"
                                                     aria-label="Engine version: activate to sort column ascending">
                                                     Combustible</th>
-                                                <th class="sorting" tabindex="0" aria-controls="tabla_vehiculos1" rowspan="1"
-                                                    colspan="1"
+                                                <th class="sorting" tabindex="0" aria-controls="tabla_vehiculos1"
+                                                    rowspan="1" colspan="1"
                                                     aria-label="Engine version: activate to sort column ascending">
                                                     Trasmisión</th>
-                                                <th class="sorting" tabindex="0" aria-controls="tabla_vehiculos1" rowspan="1"
-                                                    colspan="1"
+                                                <th class="sorting" tabindex="0" aria-controls="tabla_vehiculos1"
+                                                    rowspan="1" colspan="1"
                                                     aria-label="CSS grade: activate to sort column ascending">Acciones
                                                 </th>
                                             </tr>
@@ -84,10 +95,10 @@ include_once '../../plantillas/barra_lateral.php';
                                                 <td>
                                                     <div class="btn-group">
                                                         <button type="button" class="btn btn-secondary"
-                                                                data-toggle="modal" data-target="#modal-default1">
+                                                            data-toggle="modal" data-target="#modal-default1">
                                                             <i class="fas fa-car"></i>
                                                             <button type="button" class="btn btn-success"
-                                                                    data-toggle="modal" data-target="#modal-default">
+                                                                data-toggle="modal" data-target="#modal-default">
                                                                 <i class="fas fa-eye"></i>
                                                             </button>
                                                             <button type="button" class="btn btn-primary"><i
@@ -227,8 +238,66 @@ include_once '../../plantillas/barra_lateral.php';
             <div class="modal-body">
                 <p>
                     <img class="center"
-                         src="https://ik.imagekit.io/udcdt0pvr/wp-content/uploads/2019/08/hyundai-elantra-njautoimport-carro-8-300x225.jpeg?wave"
-                         alt="" />
+                        src="https://ik.imagekit.io/udcdt0pvr/wp-content/uploads/2019/08/hyundai-elantra-njautoimport-carro-8-300x225.jpeg?wave"
+                        alt="" />
+                <p>
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.cierre modal de imagen-->
+
+<!-- /.MODALES DE BOTONES PARA INSERTAR -->
+<div class="modal fade" id="modal-marca">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>
+                    <!-- Main content -->
+                <section class="content">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="timeline">
+
+                                <div>
+                                    <i class="fas fa-car bg-blue"></i>
+                                    <div class="timeline-item">
+
+                                        <h3 class="timeline-header"><a href="#">Registrar Marca</a> </h3>
+
+                                        <div class="timeline-body">
+                                            <!-- INICIO de primera fila -->
+                                            <div class="row">
+                                                <div class="col-sm-12">
+                                                    <!-- text input -->
+                                                    <div class="form-group">
+                                                        <label>Nombre de Marca</label>
+                                                        <input type="text" class="form-control"
+                                                            placeholder="Digite nombre de marca">
+                                                    </div>
+                                                </div>
+                                            </div>
+
+
+                                        </div>
+                                        <div class="timeline-footer" style="text-align: right;">
+                                            <a class="btn btn-info btn-sm" style="color: white">Guardar</a>
+                                            <a class="btn btn-danger btn-sm" style="color: white">Cancelar</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
                 <p>
             </div>
 
@@ -237,7 +306,135 @@ include_once '../../plantillas/barra_lateral.php';
     </div>
     <!-- /.modal-dialog -->
 </div>
-<!-- /.modal -->
+<!-- /.Cierre de MODAL MARCA -->
+
+
+<!-- /.MODALES DE BOTONES PARA INSERTAR -->
+<div class="modal fade" id="modal-modelo">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>
+                    <!-- Main content -->
+                <section class="content">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="timeline">
+
+                                <div>
+                                    <i class="fas fa-car bg-blue"></i>
+                                    <div class="timeline-item">
+
+                                        <h3 class="timeline-header"><a href="#">Registrar Modelo</a> </h3>
+
+                                        <div class="timeline-body">
+                                            <!-- INICIO de primera fila -->
+                                            <div class="row">
+                                                <div class="col-sm-12">
+                                                    <!-- text input -->
+                                                    <div class="form-group">
+                                                        <label>Seleccione Marca</label>
+                                                        <select class="form-control">
+
+                                                            <option value="">Toyota</option>
+                                                            <option value="">Nissan</option>
+                                                            <option value="">Hyundai</option>
+                                                        </select>
+                                                    </div>
+                                                    <div class="form-group">
+                                                        <label>Nombre de Modelo</label>
+                                                        <input type="text" class="form-control"
+                                                            placeholder="Digite nombre de modelo">
+                                                    </div>
+                                                </div>
+                                            </div>
+
+
+                                        </div>
+                                        <div class="timeline-footer" style="text-align: right;">
+                                            <a class="btn btn-info btn-sm" style="color: white">Guardar</a>
+                                            <a class="btn btn-danger btn-sm" style="color: white">Cancelar</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <p>
+            </div>
+
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.Cierre de MODAL MODELO -->
+
+<!-- /.MODALES DE BOTONES PARA INSERTAR -->
+<div class="modal fade" id="modal-categoria">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>
+                    <!-- Main content -->
+                <section class="content">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="timeline">
+
+                                <div>
+                                    <i class="fas fa-car bg-blue"></i>
+                                    <div class="timeline-item">
+
+                                        <h3 class="timeline-header"><a href="#">Registrar Categoria</a> </h3>
+
+                                        <div class="timeline-body">
+                                            <!-- INICIO de primera fila -->
+                                            <div class="row">
+                                                <div class="col-sm-12">
+                                                    <!-- text input -->
+                                                    <div class="form-group">
+                                                        <label>Nombre de Categoria</label>
+                                                        <input type="text" class="form-control"
+                                                            placeholder="Digite nombre de categoria">
+                                                    </div>
+                                                </div>
+                                            </div>
+
+
+                                        </div>
+                                        <div class="timeline-footer" style="text-align: right;">
+                                            <a class="btn btn-info btn-sm" style="color: white">Guardar</a>
+                                            <a class="btn btn-danger btn-sm" style="color: white">Cancelar</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <p>
+            </div>
+
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.Cierre de MODAL CATEGORIA -->
 
 
 <?php
@@ -246,11 +443,11 @@ include_once '../../plantillas/barra_lateral.php';
 
 
 <script>
-    $(function () {
-        $("#tabla_vehiculos1").DataTable({
-            "responsive": true,
-            "autoWidth": false,
-        });
-
+$(function() {
+    $("#tabla_vehiculos1").DataTable({
+        "responsive": true,
+        "autoWidth": false,
     });
+
+});
 </script>
