e2aa52f11d73d0ac192af1d181d1504760727036
JcMoz <31970838+JcMoz@users.noreply.github.com>
2020-11-04 13:22:32 -0600

    PULIENDO LAS PREGUNTAS

--- SistemaAgencia/vistas/Asesoria/registroPreguntas.php
+++ SistemaAgencia/vistas/Asesoria/registroPreguntas.php
@@ -165,6 +165,7 @@ include_once '../../plantillas/barra_lateral.php';
 <div id="script"></div>
 <script type="text/javascript" src="<?= $base_url?>js/controladores/asesorias/insertar-pregunta-app.js"></script>
 <script type="text/javascript" src="<?= $base_url?>js/controladores/asesorias/creacion_registros.js"></script>
+<script type="text/javascript" src="<?= $base_url?>js/controladores/asesorias/ramas_automaticas.js"></script>
 <!--<script type="text/javascript" src="<?= $base_url?>js/controladores/asesorias/combobox-ramas.js"></script>-->
 
 <script src="<?= $base_url ?>plugins/sweetalert2/sweetalert2.min.js"></script>
