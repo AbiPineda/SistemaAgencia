ba27d2588eac48c7871b3d962f5ac5993b02432e
JcMoz <31970838+JcMoz@users.noreply.github.com>
2020-11-13 22:27:50 -0600

    pesonalizada

--- SistemaAgencia/js/controladores/encomienda/creacion_formulario.js
+++ SistemaAgencia/js/controladores/encomienda/creacion_formulario.js
@@ -25,7 +25,7 @@ $(function () {
                                         '<div class="form-group">'+
                                         '<label for="cars">Forma de operar</label>'+
                                         '<input name="forma" value="" id="forma" type="text" class="form-control" disabled="true">'+
-                                        '<input name="nombre_empresa" value="" id="nombre_empresa" type="text">'+
+                                        '<input name="nombre_empresa" value="" id="nombre_empresa" type="hidden">'+
                                         '</div></div>'+
                                     '<div class="col-sm-8">'+
                                         '<div class="form-group">'+
@@ -35,7 +35,7 @@ $(function () {
                                     '<div class="col-sm-6">'+
                                         '<div class="form-group">'+
                                             '<label>Punto de referencia</label>'+
-                                            '<input name="punto_referencia" type="text" class="form-control" placeholder="Introduzca el telefono">'+
+                                            '<input name="punto_referencia" type="text" class="form-control" placeholder="Introduzca el punto de referencia">'+
                                         '</div></div>'+
                                     '<div class="col-sm-6">'+
                                         '<div class="form-group">'+
@@ -56,8 +56,30 @@ $(function () {
                                             '<label for="cars">Ingrese el costo</label>'+
                                             '<input name="costo" type="text" class="form-control" placeholder="Introduzca el costo">'+
                                         '</div></div></div></form></div></div>');
-                $('#forma').val(cod);
-                $('#nombre_empresa').val(empre);
+                ///para extraer un par de datos que necesito
+            $.ajax({
+            type: "GET",
+            url: URL_SERVIDOR+"Empresa/empresas?nombre_empresa="+empre,
+            async: false,
+            dataType: "json",
+            success: function(data) {
+
+               
+                $.each(data.empresa, function(i, index) {
+                    $('#forma').val(index.forma_operacion);
+                    $('#nombre_empresa').val(index.nombre_empresa);
+                });
+            },
+            error: function(err) {
+              /* const Toast = Swal.mixin();
+            Toast.fire({
+                title: 'Oops...',
+                icon: 'error',
+                text:'No hay Ramas para mostrar',
+                showConfirmButton: true,
+            });*/
+            }
+        });
 
                 $botones.append(' <i class="fas fa-save bg-gradient-lightblue"></i>'+
                             '<div class="timeline-item">'+
@@ -74,7 +96,7 @@ $(function () {
                $('#script').html('<script type="text/javascript" src="../../js/controladores/encomienda/insertar-estandar.js">');
                }
 
-               if (cod=='Personalizada') {
+               if (cod=='Personalizado') {
                	   $mostrar.append('   <i class="fas fa-box-open bg-green"></i>'+
                         '<div class="timeline-item">'+
                          '<h3 class="timeline-header"><a href="#">Ingrese su configuracion estándar</a></h3>'+
@@ -82,21 +104,26 @@ $(function () {
                             '<div class="timeline-body">'+
                             '<form id="register-form" name="register-form" onsubmit="return false">'+
                                 '<div class="row">'+
-                                    '<div class="col-sm-4">'+
+                                    '<div class="col-sm-5">'+
                                         '<div class="form-group">'+
                                         '<label for="cars">Forma de operar</label>'+
                                         '<input name="forma" value="" id="forma" type="text" class="form-control" disabled="true">'+
-                                         '<input name="nombre_empresa" value="" id="nombre_empresa" type="text">'+
+                                         '<input name="nombre_empresa" value="" id="nombre_empresa" type="hidden">'+
                                         '</div></div>'+
-                                    '<div class="col-sm-8">'+
+                                        '<div class="col-sm-5">'+
+                                        '<div class="form-group">'+
+                                        '<label for="cars">Libras o por unidad</label>'+
+                                        '<input name="unidad" value="" id="unidad" type="text" class="form-control" disabled="true">'+
+                                        '</div></div>'+
+                                    '<div class="col-sm-7">'+
                                         '<div class="form-group">'+
                                             '<label>Dirección</label>'+
                                             '<input name="direccion" type="text" class="form-control" placeholder="Introduzca la dirección" autocomplete="off">'+
                                         '</div></div>'+
-                                    '<div class="col-sm-6">'+
+                                    '<div class="col-sm-5">'+
                                         '<div class="form-group">'+
                                             '<label>Punto de referencia</label>'+
-                                            '<input name="punto_referencia" type="text" class="form-control" placeholder="Introduzca el telefono">'+
+                                            '<input name="punto_referencia" type="text" class="form-control" placeholder="Introduzca el punto de referencia">'+
                                         '</div></div>'+
                                     '<div class="col-sm-6">'+
                                         '<div class="form-group">'+
@@ -112,31 +139,54 @@ $(function () {
                                                 '<option value="">Seleccione</option>'+
                                             '</select></div>'+
                                     '</div>'+
-                                    '<div class="col-sm-5">'+
+                                    '<div class="col-sm-4">'+
                                        '<div class="form-group">'+
                                             '<label for="cars">Ingrese el producto</label>'+
-                                            '<input name="producto" type="text" class="form-control" placeholder="Introduzca el producto">'+
+                                            '<input name="nombre_producto" type="text" class="form-control" placeholder="Introduzca el producto">'+
                                         '</div>'+
                                     '</div>'+
-                                    '<div class="col-sm-6">'+
+                                    '<div class="col-sm-4">'+
                                         '<div class="form-group">'+
                                            '<label for="cars">Seleccione caja o bolsa</label>'+
-                                              '<select name="caja" id="caja" class="form-control">'+
+                                              '<select name="envoltura" id="caja" class="form-control">'+
                                                 '<option value="">Seleccione</option>'+
                                                  '<option value="caja">Caja</option>'+
                                                   '<option value="bolsa">Bolsa</option>'+
 
                                             '</select>'+
                                         '</div></div>'+
-                                    '<div class="col-sm-5">'+
+                                    '<div class="col-sm-4">'+
                                        '<div class="form-group">'+
                                             '<label for="cars">Ingrese el costo</label>'+
                                             '<input name="costo" type="text" class="form-control" placeholder="Introduzca el costo">'+
                                         '</div>'+
                                     '</div>'+
                                 '</div></form></div></div>');
-                $('#forma').val(cod);
-                $('#nombre_empresa').val(empre);
+                   ///para extraer un par de datos que necesito
+            $.ajax({
+            type: "GET",
+            url: URL_SERVIDOR+"Empresa/empresas?nombre_empresa="+empre,
+            async: false,
+            dataType: "json",
+            success: function(data) {
+
+               
+                $.each(data.empresa, function(i, index) {
+                    $('#forma').val(index.forma_operacion);
+                    $('#nombre_empresa').val(index.nombre_empresa);
+                    $('#unidad').val(index.unidad);
+                });
+            },
+            error: function(err) {
+              /* const Toast = Swal.mixin();
+            Toast.fire({
+                title: 'Oops...',
+                icon: 'error',
+                text:'No hay Ramas para mostrar',
+                showConfirmButton: true,
+            });*/
+            }
+        });
 
                 $botones.append(' <i class="fas fa-save bg-green"></i>'+
                             '<div class="timeline-item">'+
@@ -146,12 +196,13 @@ $(function () {
                                         '<div class="col-lg-1"></div><div class="col-lg-10"></div>'+
                                           '<div class="col-lg-1"></div>'+
                                         '<div style="text-align: right;width:577px;margin-top:-8px;">'+
-                                 '<a class="btn btn-primary btn-sm" id="btn-preguntas" style="color: white">Guardar</a>'+
+                                 '<a class="btn btn-primary btn-sm" id="btn-perso" style="color: white">Guardar</a>'+
                                 '<a class="btn btn-danger btn-sm" style="color: white">Cancelar</a>'+
                                 '</div></div></div></div>');
 
-                 $('#script').html('<script type="text/javascript" src="../../js/controladores/encomienda/deptos.js">');
-              
+                $('#script').html('<script type="text/javascript" src="../../js/controladores/encomienda/deptos.js">');
+               $('#script').html('<script type="text/javascript" src="../../js/controladores/encomienda/insertar-personalizada.js">');
+               
                }
 
           
