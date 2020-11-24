2a6cc87b8a50e0d92b1a76f304da4287929b6025
Abigail <pineverdi@gmail.com>
2020-11-24 16:12:18 -0600

    tipo de viaje

---
+++ SistemaAgencia/js/controladores/vuelos/insertarTipoViaje.js
@@ -0,0 +1,51 @@
+$("#btnAgregarViaje").on('click', function(e) {
+
+    e.preventDefault();
+    $.ajax({
+        url: URL_SERVIDOR + "tipo_viaje/viajes",
+        method: 'POST',
+        data: $("#register-viaje").serialize()
+
+    }).done(function(response) {
+
+        $("#modal-tipoViaje").modal('toggle');
+
+        document.getElementById("register-viaje").reset();
+
+        const Toast = Swal.mixin();
+        Toast.fire({
+            title: 'Exito...',
+            icon: 'success',
+            text: response.mensaje,
+            showConfirmButton: true,
+        }).then((result) => {
+            //TODO BIEN Y RECARGAMOS LA PAGINA 
+            location.reload();
+        });
+    }).fail(function(response) {
+        //SI HUBO UN ERROR EN LA RESPUETA REST_Controller::HTTP_BAD_REQUEST
+        let respuestaDecodificada = JSON.parse(response.responseText);
+        let listaErrores = "";
+
+        if (respuestaDecodificada.errores) {
+            ///ARREGLO DE ERRORES 
+            let erroresEnvioDatos = respuestaDecodificada.errores;
+            for (mensaje in erroresEnvioDatos) {
+                listaErrores += erroresEnvioDatos[mensaje] + "\n";
+                //toastr.error(erroresEnvioDatos[mensaje]);
+            };
+        } else {
+            listaErrores = respuestaDecodificada.mensaje
+        }
+        const Toast = Swal.mixin();
+        Toast.fire({
+            title: 'Oops...',
+            icon: 'error',
+            text: listaErrores,
+            showConfirmButton: true,
+        });
+
+    })
+
+
+});
--- SistemaAgencia/vistas/vuelos/mostrarTipoViaje.php
+++ SistemaAgencia/vistas/vuelos/mostrarTipoViaje.php
@@ -1,9 +1,8 @@
-<?php
-include_once '../../config/parametros.php';
-include_once '../../plantillas/cabecera.php';
-include_once  '../../plantillas/navbar.php';
-include_once '../../plantillas/barra_lateral.php';
-?>
+<?php include_once '../../config/parametros.php'; ?>
+<?php include_once '../../plantillas/cabecera.php'; ?>
+<!-- COLORAR ESTILOS ADICIONALES AQUI -->
+<link href="<?= $base_url ?>plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css" all rel="stylesheet"
+    type="text/css" />
 <style>
 .center {
     display: block;
@@ -12,6 +11,11 @@ include_once '../../plantillas/barra_lateral.php';
     width: 75%;
 }
 </style>
+<!-- CONTINUAMOS CON LA INICIALIZACION -->
+<?php include_once  '../../plantillas/navbar.php'; ?> <?php include_once '../../plantillas/barra_lateral.php'; ?>
+
+
+
 <div class="content-wrapper" style="min-height: 1185.73px;">
     <!-- Content Header (Page header) -->
     <section class="content-header">
@@ -41,69 +45,37 @@ include_once '../../plantillas/barra_lateral.php';
                         <h3 class="card-title">Datos Generales</h3>
                         <div class="timeline-footer" style="text-align: right;">
                             <a class="btn btn-info btn-sm" style="color: white" data-toggle="modal"
-                                data-target="#modal-clase">Nuevo Tipo de Viaje</a>
+                                data-target="#modal-tipoViaje">Nuevo Tipo de Viaje</a>
+
+
                         </div>
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body">
-                        <div id="tabla_vehiculos1_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                        <div id="" class="dataTables_wrapper dt-bootstrap4">
                             <div class="row">
                                 <div class="col-sm-12">
-                                    <table id="tabla_vehiculos1"
-                                        class="table table-bordered table-striped dataTable dtr-inline" role="grid"
-                                        aria-describedby="tabla_vehiculos1_info">
-                                        <thead>
-                                            <tr role="row" style="text-align: center;">
-                                                <th class="sorting_asc" tabindex="0" aria-controls="tabla_vehiculos1"
-                                                    rowspan="1" colspan="1" aria-sort="ascending"
-                                                    aria-label="Rendering engine: activate to sort column descending">
-                                                    Nombre</th>
-                                                <th class="sorting" tabindex="0" aria-controls="tabla_vehiculos1"
-                                                    rowspan="1" colspan="1"
-                                                    aria-label="Browser: activate to sort column ascending">
-                                                    Descripción</th>
-
+                                    <table id="tabla_tiposViaje" class="table table-bordered table-striped">
+                                        <thead style="text-align: center;">
+                                            <tr>
+                                                <th>Nombre</th>
+                                                <th>Descripción</th>
+                                                <th>Acciones</th>
                                             </tr>
                                         </thead>
-                                        <tbody>
-                                            <tr role="row" class="odd" style="text-align: left;">
-                                                <td tabindex="0" class="sorting_1">Cultural</td>
-                                                <td tabindex="0" class="sorting_1">
-                                                    Es uno de los tipos de turismo más conocidos y practicados en la
-                                                    actualidad, cuando la gente se preocupa por conocer la historia y la
-                                                    cultura de otras ciudades, que normalmente viven diferente: el
-                                                    idioma, las costumbres, los lugares, los horarios, la gastronomía,
-                                                    los monumentos.
-                                                    <br>
-                                                    Aunque pueda parecer similar, todo es diferente y
-                                                    esto atrae a una masa ingente de turistas que quieren conocer cosas
-                                                    nuevas.
-                                                    
-                                                </td>
+                                        <!-- /.inicio de loading -->
+                                        <div class="overlay-wrapper">
+                                            <div id="loading" class="overlay"><i
+                                                    class="fas fa-3x fa-sync-alt fa-spin"></i>
 
-                                                <td>
-                                                    <div class="btn-group">
+                                                <div class="text-bold pt-2">Cargando...
+                                                </div>
+                                            </div>
+                                            <tbody id="tableBody" style="text-align: center;">
+                                            </tbody>
+                                        </div>
+                                        <!-- /.fin de loading -->
 
-                                                        <button type="button" class="btn btn-primary"><i
-                                                                class="fas fa-edit"></i></button>
-                                                        <button type="button" class="btn btn-danger"><i
-                                                                class="fas fa-trash-alt"></i></button>
-                                                        <button type="button" class="btn btn-warning"><i
-                                                                class="fas fa-long-arrow-alt-down"
-                                                                style="color: white"></i></button>
-                                                    </div>
-                                                </td>
-
-                                            </tr>
-                                        </tbody>
-                                        <tfoot>
-                                            <tr style="text-align: center;">
-                                                <th rowspan="1" colspan="1">Nombre</th>
-                                                <th rowspan="1" colspan="1">Descripción</th>
-
-
-                                            </tr>
-                                        </tfoot>
                                     </table>
                                 </div>
                             </div>
@@ -118,86 +90,83 @@ include_once '../../plantillas/barra_lateral.php';
         </div>
         <!-- /.row -->
     </section>
-    <!-- /.content -->
 </div>
-<div class="modal fade" id="modal-clase">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <p>
-                    <!-- Main content -->
-                <section class="content">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="timeline">
-
-                                <div>
-                                    <i class="fas fa-plane bg-blue"></i>
-                                    <div class="timeline-item">
-
-                                        <h3 class="timeline-header"><a href="#">Registrar Tipo de Viaje</a> </h3>
-
-                                        <div class="timeline-body">
-                                            <!-- INICIO de primera fila -->
-                                            <div class="row">
-                                                <div class="col-sm-12">
-                                                    <!-- text input -->
-                                                    <div class="form-group">
-                                                        <label>Tipo de Viaje</label>
-                                                        <input type="text" class="form-control"
-                                                            placeholder="Digite tipo de viaje">
-                                                    </div>
-
-                                                    <div class="form-group">
-                                                        <label>Descripción</label>
-                                                        <textarea class="form-control" rows="6"
-                                                            placeholder="Digitar aquí..."></textarea>
-                                                    </div>
-
-                                                </div>
-                                            </div>
+<!-- End Modal Mostrar-->
 
 
-                                        </div>
-                                        <div class="timeline-footer" style="text-align: right;">
-                                            <a class="btn btn-info btn-sm" style="color: white">Guardar</a>
-                                            <a class="btn btn-danger btn-sm" style="color: white">Cancelar</a>
-                                        </div>
-                                    </div>
+
+
+<form id="miFormulario" name="miFormulario" role="form" onsubmit="return false">
+    <!-- Modal EDITAR-->
+    <div class="modal fade" id="modal-editar">
+        <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+
+                <div class="overlay-wrapper">
+
+                    <div class="modal-header">
+                        <h4 class="modal-title">Modificar Tipo de Viaje</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+
+                            <div class="col-sm-12">
+                                <!-- text input -->
+                                <div class="form-group">
+                                    <label>Nombre de Tipo de Viaje</label>
+                                    <input type="text" name="nombre_tipoviaje" id="nombre_tipoviaje" class="form-control"
+                                        placeholder="Digite nombre de tipo de viaje">
+                                </div>
+                            </div>
+
+                            <div class="col-sm-12">
+                                <!-- text input -->
+                                <div class="form-group">
+                                    <label>Descripción</label>
+                                    <textarea class="form-control" rows="3" name="descripcion" id="descripcion"
+                                        placeholder="Describir.."></textarea>
                                 </div>
                             </div>
                         </div>
+
+
                     </div>
-                </section>
-                <p>
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                        <button name="btnActualizar" id="btnActualizar" class="btn btn-info btn-sm"
+                            style="color: white">Actualizar</button>
+                    </div>
+                </div>
             </div>
-
+            <!-- /.modal-content -->
         </div>
-        <!-- /.modal-content -->
+        <!-- /.modal-dialog -->
     </div>
-    <!-- /.modal-dialog -->
-</div>
-<!-- /.modal -->
+    <!-- End Modal EDITAR-->
+</form>
 
 
 
-<?php
-  include_once '../../plantillas/footer.php';
-?>
+
+<!-- /.MODALES DE BOTONES PARA INSERTAR -->
+
+<!-- /.Cierre de MODAL MARCA -->
 
 
-<script>
-$(function() {
-    $("#tabla_vehiculos1").DataTable({
-        "responsive": true,
-        "autoWidth": false,
-    });
+<?php 
+ include_once './modal-tipoViaje.php';
+ include_once '../../plantillas/footer.php';?>
 
-});
-</script>
+<!-- SCRIPT ADICIONALES -->
+<script type="text/javascript" src="<?= $base_url?>js/controladores/conf.js"></script>
+
+<script src="<?= $base_url ?>plugins/jquery-validation/jquery.validate.min.js"></script>
+<script src="<?= $base_url ?>plugins/jquery-validation/additional-methods.min.js"></script>
+
+<script type="text/javascript" src="<?= $base_url?>js/controladores/vuelos/viajes-app.js"></script>
+<script type="text/javascript" src="<?= $base_url ?>plugins/sweetalert2/sweetalert2.min.js"></script>
+
+<?php  include_once '../../plantillas/cierre.php';?>
