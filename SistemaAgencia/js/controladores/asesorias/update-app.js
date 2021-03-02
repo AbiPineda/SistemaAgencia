2f67e918e4291773c6b36a365d7a79e16cd980d2
JcMoz <31970838+JcMoz@users.noreply.github.com>
2021-03-01 22:48:01 -0600

    detalle

--- SistemaAgencia/js/controladores/asesorias/update-app.js
+++ SistemaAgencia/js/controladores/asesorias/update-app.js
@@ -29,24 +29,13 @@
 
         }).fail(function (response) {
             //SI HUBO UN ERROR EN LA RESPUETA REST_Controller::HTTP_BAD_REQUEST
-            let respuestaDecodificada = JSON.parse(response.responseText);
-            let listaErrores = "";
-
-            if (respuestaDecodificada.errores) {
-                ///ARREGLO DE ERRORES 
-                let erroresEnvioDatos = respuestaDecodificada.errores;
-                for (mensaje in erroresEnvioDatos) {
-                    listaErrores += erroresEnvioDatos[mensaje] + "\n";
-                     //toastr.error(erroresEnvioDatos[mensaje]);
-                };
-            } else {
-                listaErrores = respuestaDecodificada.mensaje
-            }
+           
+            
             const Toast = Swal.mixin();
             Toast.fire({
                 title: 'Oops...',
                 icon: 'error',
-                text: listaErrores,
+                text: response,
                 showConfirmButton: true,
             });
 
