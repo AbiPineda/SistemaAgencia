27062f5875279e5c8f307ff8d54d2cfeedcc4db3
JcMoz <31970838+JcMoz@users.noreply.github.com>
2021-03-14 20:04:06 -0600

    validar un poco la existencia

--- SistemaAgencia/js/controladores/asesorias/validar-exist.js
+++ SistemaAgencia/js/controladores/asesorias/validar-exist.js
@@ -17,15 +17,15 @@ $(function () {
     }).done(function(response) {
 
         //alert(response.existe.id_cita);
-       let fecha=response.existe.fecha;
-       let nueva= fecha.split('-');
-
+      
        if (response.mensaje=='Existe' && response.existe.estado_cita==0) {
         const Toast = Swal.mixin();
         Swal.fire({
             title: '¡EL cliente ya ha realizado proceso migratorio!',
             text: "¿Desea modificar la información?",
             icon: 'warning',
+             showCancelButton: true,
+            cancelButtonText: "Cancelar",
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
             confirmButtonText: 'Sí!'
@@ -35,7 +35,15 @@ $(function () {
                window.location = `${URL_SISTEMA}vistas/Asesoria/Formulario.php?form=`+response.existe.id_cita;
             }
         });
+
+         document.getElementById("btnAgregar").disabled = true; 
+
        }else{
+
+       if(response.mensaje=='Existe' && response.existe.estado_cita==1){
+
+       let fecha=response.existe.fecha;
+       let nueva= fecha.split('-');
 
          const Toast = Swal.mixin();
         Swal.fire({
@@ -51,9 +59,17 @@ $(function () {
                  $("#modal_registro").modal('toggle');
 
             }
-        })
+        });
 
+         document.getElementById("btnAgregar").disabled = true; 
+
+       }else{
+         //por si se queda trabado el disabled lo habilitamos
+            document.getElementById("btnAgregar").disabled = false;
        }
+   }
+
+
     }).fail(function(response) {
         //SI HUBO UN ERROR EN LA RESPUETA REST_Controller::HTTP_BAD_REQUEST
         let respuestaDecodificada = JSON.parse(response.responseText);
