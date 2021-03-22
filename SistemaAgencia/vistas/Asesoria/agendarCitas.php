c168d0c7cbeb70eaa31d15f3383f7818041db6df
JcMoz <31970838+JcMoz@users.noreply.github.com>
2021-03-21 20:34:09 -0600

    agendar cita

--- SistemaAgencia/vistas/Asesoria/agendarCitas.php
+++ SistemaAgencia/vistas/Asesoria/agendarCitas.php
@@ -22,7 +22,7 @@
       <div class="container-fluid">
          <div class="row mb-2">
             <div class="col-sm-6">
-               <h1>Programación de Citas</h1>
+               <h1>Agendar Citas</h1>
             </div>
             <div class="col-sm-6">
                <ol class="breadcrumb float-sm-right">
