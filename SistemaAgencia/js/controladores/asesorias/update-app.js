9ceac6deefa0cdf877a255886bbd580799e67c10
JcMoz <31970838+JcMoz@users.noreply.github.com>
2021-05-12 20:42:31 -0600

    comenzando a hacer el editar foto de la asesoria

--- SistemaAgencia/js/controladores/asesorias/update-app.js
+++ SistemaAgencia/js/controladores/asesorias/update-app.js
@@ -1,4 +1,51 @@
- $("#btnActualizar").on('click', function(e) {
+ $(document).ready(function (){
+
+inicializarGaleria();
+inicializarFoto();
+
+
+    //***************para la galeria******************
+     function inicializarGaleria() {
+        // ESTO ES PARA INICIALIZAR EL ELEMENTO DE SUBIDA DE FOTOS (EN ESTE CASO UNA GALERIA )
+        $('#fotos').fileinput({
+            theme: 'fas',
+            language: 'es',
+            //uploadUrl: '#',
+            showUpload: false,
+            //showCaption: false,
+            maxFileSize: 2000,
+            maxFilesNum: 10,
+            allowedFileExtensions: ['jpg', 'png', 'gif'],
+            required: true,
+            uploadAsync: false,
+            showClose: false,
+        });
+    }
+
+    function inicializarFoto() {
+        // ESTO ES PARA INICIALIZAR EL ELEMENTO DE SUBIDA DE UNA UNICA FOTO
+        $('#foto').fileinput({
+            theme: 'fas',
+            language: 'es',
+            required: true,
+            maxFileSize: 2000,
+            maxFilesNum: 10,
+            showUpload: false,
+            showClose: false,
+            showCaption: true,
+            browseLabel: '',
+            removeLabel: '',
+            //removeIcon: '<i class="glyphicon glyphicon-remove"></i>',
+            removeTitle: 'Cancel or reset changes',
+            elErrorContainer: '#kv-avatar-errors-1',
+            msgErrorClass: 'alert alert-block alert-danger',
+            defaultPreviewContent: '<img src="../../img/avatar.png" alt="Your Avatar">',
+            layoutTemplates: { main2: '{preview} {remove} {browse}' },
+            allowedFileExtensions: ["jpg", "png", "gif"]
+        });
+    }
+
+    $("#btnActualizar").on('click', function(e) {
 
         e.preventDefault();
         // recolectarDatos();
@@ -41,6 +88,13 @@
                 showConfirmButton: true,
             });
 
-        })
+        });
 
     });
+
+
+});
+
+
+
+ 
