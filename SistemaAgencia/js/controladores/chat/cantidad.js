d793bc2705d10eb34c64aebabc8966ecec19fe2f
Boris Ricardo Miranda <brmiranda009@gmail.com>
2021-05-17 11:50:46 -0600

    avanzando en lo del chat

--- SistemaAgencia/js/controladores/chat/cantidad.js
+++ SistemaAgencia/js/controladores/chat/cantidad.js
@@ -8,10 +8,30 @@ function obenerChats() {
    // });
 
 
+   $.ajax({
+      url: "http://localhost/API-REST-PHP/Usuario/obtenerAdminByChat",
+      method: "GET"
+   }).done(function (response) {
+      // //CARGAMOS EL COSTO AL INPUT
+      let time = new Date(response.ultimaConexion);
+      db.collection("chat")
+         .where("user_1_uuid",'!=', response.uuid)
+         .where("time", ">", time)
+         .get()
+         .then((snapshot) => {
+            console.log(snapshot.size);
+            $('#mensajesPendientes').text(snapshot.size);
+         })
+         .catch((e) => {
+            console.log(e);
+         });
 
-   db.collection("chat").get().then((snapshot) => {
-      $('#mensajesPendientes').text(snapshot.size);
+      ;
+   }).fail(function (response) {
+      console.log(response);
+
    });
+
 }
 
 obenerChats();
--- SistemaAgencia/js/controladores/chat/chat.js
+++ SistemaAgencia/js/controladores/chat/chat.js
@@ -17,43 +17,6 @@ let fotoEmisor;
 getUsers();
 
 
-function getUsers() {
-  $.ajax({
-    url: "http://localhost/API-REST-PHP/Usuario/obtenerUsuarioByChat",
-    method: "GET",
-    success: function (response) {
-      if (!response.error) {
-        uid_administrador = response.administrador.uuid;
-        fotoEmisor = response.administrador.foto;
-        let users = response.usuarios;
-        let usersHTML = "";
-        $.each(users, function (index, value) {
-          //SE RECORREN TODOS LOS USUARIOS Y SE PONEN EN LA LISTA
-          //DE CHATS CON ECEPCCION DEL MISMO LA FOTO DEL USUARIO ACTUAL
-          if (uid_administrador != value.uuid) {
-            usersHTML +=
-              '<div class="user" uuid="' +
-              value.uuid +
-              '">' +
-              // '<img  src="' + value.foto + '" class="user-image"/>' +
-              '<div class="user-image"><img  src="' + value.foto + '" class="user-image"/></div>' +
-              '<div class="user-details">' +
-              "<span><strong>" +
-              value.nombre +
-              '<span class="count"></span></strong></span>' +
-              "<span></span>" +
-              "</div>" +
-              "</div>";
-          }
-        });
-        // DIBUJAMOS LOS USUARIOS EN LA BARRA LATERAL
-        $(".users").html(usersHTML);
-      } else {
-        console.log(response.message);
-      }
-    },
-  });
-}
 
 $(document.body).on("click", ".user", function () {
   //ESTA PARTE ES PARA EL EFECTO CSS DE CAMBIO
@@ -92,125 +55,6 @@ $(document.body).on("click", ".user", function () {
 
 });
 
-$(document).on('click', '#cerrarSesion', function () {
-  firebase
-    .auth()
-    .signOut()
-    .then(function () {
-      console.log("Logout");
-      window.location.href = URL_SISTEMA;
-    })
-    .catch(function (error) {
-      // An error happened.
-      console.log("Logout Fail");
-      window.location.href = URL_SISTEMA;
-    });
-});
-
-function actualizarFecha(uuid) {
-  $.ajax({
-    url: "http://localhost/API-REST-PHP/Usuario/updateFecha",
-    method: "PUT",
-    data: { uuid },
-    success: function (resp) {
-      // console.log(resp);
-    },
-  });
-}
-
-$(".send-btn").on("click", function () {
-  enviarMensaje();
-});
-
-///CREA UN LISTERNER INTERNAMENTE PARA CREAR LOS NUEVOS MENSAJES EN PANTALLA
-function realTime() {
-  referenciaRT = db.collection("chat")
-    .where("chat_uuid", "==", chat_uuid)
-    .orderBy("time", "desc")
-    .limit(15)
-    .onSnapshot(function (snapshot) {
-      newMessage = "";
-      snapshot.docChanges().slice().reverse().forEach(function (change) {
-
-        if (!proximaConsulta) {
-          let lastVisible = snapshot.docs[snapshot.docs.length - 1];
-          proximaConsulta = db.collection("chat")
-            .where("chat_uuid", "==", chat_uuid)
-            .orderBy("time", "desc")
-          .startAfter(lastVisible)
-          .limit(2);
-        }
-
-        if (change.type === "added") {
-          if (change.doc.data().user_1_uuid == uid_administrador) {
-            ///debe de mostrar la foto de quien esta enviando el mensaje EMISOR
-            newMessage +=
-              '<div class="message-block received-message">' +
-              '<div class="user-icon"><img  src="' + fotoEmisor + '" class="user-icon"/></div>' +
-              '<div class="message">' +
-              change.doc.data().message +
-              "</div>" +
-              "</div>";
-          } else {
-            if (activarSonido) {
-              let audio = new Audio('new-ticket.mp3');
-              audio.play();
-            }
-            //debe de mostrar la foto de quien se esta recibiendo el mensaje (la imagen que aca de darse click) RECEPTOR
-            newMessage +=
-              '<div class="message-block ">' +
-              '<div class="user-icon"><img  src="' + fotoReceptor + '" class="user-icon"/></div>' +
-              '<div class="message">' +
-              change.doc.data().message +
-              "</div>" +
-              "</div>";
-          }
-        }
-        if (change.type === "modified") {
-        }
-        if (change.type === "removed") {
-        }
-      });
-      if (chatHTML != newMessage) {
-        $(".message-container").append(newMessage);
-      }
-      activarSonido = true;
-      $(".chats").scrollTop($(".chats")[0].scrollHeight);
-    });
-}
-
-$('#message-input').on('keypress', function (e) {
-  let keycode = (e.keyCode ? e.keyCode : e.which);
-  if (keycode == '13') {
-    enviarMensaje();
-    e.preventDefault();
-    return false;
-  }
-});
-
-function enviarMensaje() {
-  let message = $(".message-input").val();
-  if (message != "") {
-    db.collection("chat")
-      .add({
-        message: message,
-        user_1_uuid: uid_administrador,
-        user_2_uuid: uid_cliente,
-        chat_uuid: chat_uuid,
-        user_1_isView: 0,
-        user_2_isView: 0,
-        time: new Date(),
-      })
-      .then(function (docRef) {
-        $(".message-input").val("");
-        // console.log("Document written with ID: ", docRef.id);
-        actualizarFecha(uid_cliente);
-      })
-      .catch(function (error) {
-        console.error("Error adding document: ", error);
-      });
-  }
-}
 $('#chats').scroll(function () {
   if ($('#chats').scrollTop() == 0) {
     console.log("cargar nuevos datos");
@@ -269,3 +113,143 @@ $('#chats').scroll(function () {
     }
   }
 });
+
+$('#message-input').on('keypress', function (e) {
+  let keycode = (e.keyCode ? e.keyCode : e.which);
+  if (keycode == '13') {
+    enviarMensaje();
+    e.preventDefault();
+    return false;
+  }
+});
+$(".send-btn").on("click", function () {
+  enviarMensaje();
+});
+
+function getUsers() {
+  $.ajax({
+    url: "http://localhost/API-REST-PHP/Usuario/obtenerUsuarioByChat",
+    method: "GET",
+    success: function (response) {
+      if (!response.error) {
+        uid_administrador = response.administrador.uuid;
+        fotoEmisor = response.administrador.foto;
+        let users = response.usuarios;
+        let usersHTML = "";
+        $.each(users, function (index, value) {
+          //SE RECORREN TODOS LOS USUARIOS Y SE PONEN EN LA LISTA
+          //DE CHATS CON ECEPCCION DEL MISMO LA FOTO DEL USUARIO ACTUAL
+          if (uid_administrador != value.uuid) {
+            usersHTML +=
+              '<div class="user" uuid="' +
+              value.uuid +
+              '">' +
+              // '<img  src="' + value.foto + '" class="user-image"/>' +
+              '<div class="user-image"><img  src="' + value.foto + '" class="user-image"/></div>' +
+              '<div class="user-details">' +
+              "<span><strong>" +
+              value.nombre +
+              '<span class="count"></span></strong></span>' +
+              "<span></span>" +
+              "</div>" +
+              "</div>";
+          }
+        });
+        // DIBUJAMOS LOS USUARIOS EN LA BARRA LATERAL
+        $(".users").html(usersHTML);
+        actualizarFecha(uid_administrador);
+      } else {
+        console.log(response.message);
+      }
+    },
+  });
+}
+///CREA UN LISTERNER INTERNAMENTE PARA CREAR LOS NUEVOS MENSAJES EN PANTALLA
+function realTime() {
+  referenciaRT = db.collection("chat")
+    .where("chat_uuid", "==", chat_uuid)
+    .orderBy("time", "desc")
+    .limit(15)
+    .onSnapshot(function (snapshot) {
+      newMessage = "";
+      snapshot.docChanges().slice().reverse().forEach(function (change) {
+
+        if (!proximaConsulta) {
+          let lastVisible = snapshot.docs[snapshot.docs.length - 1];
+          proximaConsulta = db.collection("chat")
+            .where("chat_uuid", "==", chat_uuid)
+            .orderBy("time", "desc")
+            .startAfter(lastVisible)
+            .limit(2);
+        }
+
+        if (change.type === "added") {
+          if (change.doc.data().user_1_uuid == uid_administrador) {
+            ///debe de mostrar la foto de quien esta enviando el mensaje EMISOR
+            newMessage +=
+              '<div class="message-block received-message">' +
+              '<div class="user-icon"><img  src="' + fotoEmisor + '" class="user-icon"/></div>' +
+              '<div class="message">' +
+              change.doc.data().message +
+              "</div>" +
+              "</div>";
+          } else {
+            if (activarSonido) {
+              let audio = new Audio('new-ticket.mp3');
+              audio.play();
+            }
+            //debe de mostrar la foto de quien se esta recibiendo el mensaje (la imagen que aca de darse click) RECEPTOR
+            newMessage +=
+              '<div class="message-block ">' +
+              '<div class="user-icon"><img  src="' + fotoReceptor + '" class="user-icon"/></div>' +
+              '<div class="message">' +
+              change.doc.data().message +
+              "</div>" +
+              "</div>";
+          }
+        }
+        if (change.type === "modified") {
+        }
+        if (change.type === "removed") {
+        }
+      });
+      if (chatHTML != newMessage) {
+        $(".message-container").append(newMessage);
+      }
+      activarSonido = true;
+      $(".chats").scrollTop($(".chats")[0].scrollHeight);
+    });
+}
+function enviarMensaje() {
+  let message = $(".message-input").val();
+  if (message != "") {
+    db.collection("chat")
+      .add({
+        message: message,
+        user_1_uuid: uid_administrador,
+        user_2_uuid: uid_cliente,
+        chat_uuid: chat_uuid,
+        user_1_isView: 0,
+        user_2_isView: 0,
+        time: new Date(),
+      })
+      .then(function (docRef) {
+        $(".message-input").val("");
+        // console.log("Document written with ID: ", docRef.id);
+        actualizarFecha(uid_cliente);
+      })
+      .catch(function (error) {
+        console.error("Error adding document: ", error);
+      });
+  }
+}
+function actualizarFecha(uuid) {
+  $.ajax({
+    url: "http://localhost/API-REST-PHP/Usuario/updateFecha",
+    method: "PUT",
+    data: { uuid },
+    success: function (resp) {
+      // console.log(resp);
+    },
+  });
+}
